fa0df6d8cde34bd15f61a293cc46fbc4bd1799d0
valentina <valeagu1710@gmail.com>
2024-09-22 00:45:54 -0500

    updated title and delete tab3.pipe

--- src/app/pages/tab3.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Tab3Pipe } from './tab3.pipe';
-
-describe('Tab3Pipe', () => {
-  it('create an instance', () => {
-    const pipe = new Tab3Pipe();
-    expect(pipe).toBeTruthy();
-  });
-});
