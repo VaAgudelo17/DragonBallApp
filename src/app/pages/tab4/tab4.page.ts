057186534a6ac0314e9aeeb481d3cc50ef5d5d92
valentina <valeagu1710@gmail.com>
2024-10-06 03:33:26 -0500

    created map

--- src/app/pages/tab4/tab4.page.ts
+++ src/app/pages/tab4/tab4.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-tab4',
@@ -6,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
+  map: L.Map | undefined;
+  currentLocationMarker: L.Marker | undefined;
 
-  constructor() { }
+  constructor(private geolocation: Geolocation) {}
 
   ngOnInit() {
+    this.loadMap();
   }
 
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      if ('coords' in resp) {
+        this.map = L.map('mapId').setView([resp.coords.latitude, resp.coords.longitude], 15);
+
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+          maxZoom: 20,
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        }).addTo(this.map);
+
+
+        const icon = L.icon({
+          iconUrl: 'assets/img/gohan.png', 
+          iconSize: [40, 40],
+          iconAnchor: [20, 40],
+          popupAnchor: [0, -40]
+        });
+
+        this.currentLocationMarker = L.marker([resp.coords.latitude, resp.coords.longitude], { icon: icon })
+          .addTo(this.map)
+          .bindPopup('Estás aquí')
+          .openPopup();
+
+        this.watchPosition();
+      }
+    }).catch((error) => {
+      console.log('Error obteniendo la localización', error);
+    });
+  }
+
+  watchPosition() {
+    this.geolocation.watchPosition().subscribe((data) => {
+      if (data && 'coords' in data) {
+        this.currentLocationMarker?.setLatLng([data.coords.latitude, data.coords.longitude]);
+      }
+    }, (error) => {
+      console.log('Error al vigilar la localización', error);
+    });
+  }
 }
