000320674872d41353db4d46bb58b88a6b89f37f
Nicolas <nicolas@dlpbasculas.com>
2024-12-02 12:29:48 -0500

    Enhance SuccessModalComponent: make success message dynamic and personalized

--- src/app/components/succes-modal/success-modal.component.ts
+++ src/app/components/succes-modal/success-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -7,6 +7,8 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./success-modal.component.scss'],
 })
 export class SuccessModalComponent {
+  @Input() message: string = '';
+
   constructor(private modalController: ModalController) {}
 
   dismiss() {
